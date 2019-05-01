5d582afefa48f6f8eeb9e53abc38149f7c2e8fb7
ajkarimi7 <ajkarimi7@gmail.com>
2019-05-01 06:18:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,7 +38,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array.slice(1, array.length - 2);
+  array.slice(1, array.length - 4);
   return array
 }
 
